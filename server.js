351c3de79ba1895ea7c0e94d9f26150cec61e238
szczurdada <natalyabel99@gmail.com>
2026-05-21 20:42:57 +0200

    feature/86c9xfyu9-BE-added-messaging-and-user-search

--- server.js
+++ server.js
@@ -12,9 +12,14 @@ const postwallRouter = require("./routers/postwallRouter");
 const commentRouter = require("./routers/commentRouter");
 const friendsRouter = require("./routers/friendsRouter");
 const likesRouter = require("./routers/likesRouter");
+const chatRouter = require("./routers/chatRouter");
+const Message = require("./models/Message");
 
 const app = express();
-const PORT = process.env.PORT || 3005;
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+  cors: { origin: "*" },
+});
 
 app.use(cors());
 app.use(express.json());
@@ -27,11 +32,29 @@ app.use("/postwall", postwallRouter);
 app.use("/comments", commentRouter);
 app.use("/friends", friendsRouter);
 app.use("/likes", likesRouter);
+app.use("/chat", chatRouter);
+
+io.on("connection", (socket) => {
+  socket.on("join", (roomId) => socket.join(roomId));
+
+  socket.on("message", async ({ roomId, senderId, text }) => {
+    try {
+      const message = await Message.create({ roomId, sender: senderId, text });
+      const populated = await message.populate("sender", "username name avatar");
+
+      io.to(roomId).emit("message", populated);
+    } catch (e) {
+      console.error(e);
+    }
+  });
+});
 
 const start = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
-    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    http.listen(process.env.PORT, () =>
+      console.log(`Server started on port ${process.env.PORT}`),
+    );
   } catch (e) {
     console.error(e);
   }
