2e6d51881a9a3d6adaa4002b91b10961de3fc1ba
szczurdada <natalyabel99@gmail.com>
2026-09-22 22:23:51 +0200

    fix/12487v2b4qw-BE-merged-friend-request-accept-reject

--- src/utils/friends.js
+++ src/utils/friends.js
@@ -1,3 +1,5 @@
+const User = require("../models/User");
+
 const containsId = (list, id) => {
   for (const item of list) {
     if (item.toString() === id.toString()) return true;
@@ -5,4 +7,54 @@ const containsId = (list, id) => {
   return false;
 };
 
-module.exports = { containsId };
+const linkAsFriends = async (userId, friendId, session) => {
+  await User.findByIdAndUpdate(
+    userId,
+    {
+      $addToSet: {
+        friends: friendId,
+        following: friendId,
+        followers: friendId,
+      },
+    },
+    { session },
+  );
+  await User.findByIdAndUpdate(
+    friendId,
+    {
+      $addToSet: {
+        friends: userId,
+        following: userId,
+        followers: userId,
+      },
+    },
+    { session },
+  );
+};
+
+const unlinkAsFriends = async (userId, friendId, session) => {
+  await User.findByIdAndUpdate(
+    userId,
+    {
+      $pull: {
+        friends: friendId,
+        following: friendId,
+        followers: friendId,
+      },
+    },
+    { session },
+  );
+  await User.findByIdAndUpdate(
+    friendId,
+    {
+      $pull: {
+        friends: userId,
+        following: userId,
+        followers: userId,
+      },
+    },
+    { session },
+  );
+};
+
+module.exports = { containsId, linkAsFriends, unlinkAsFriends };
